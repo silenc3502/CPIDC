7c08e9ddf78b8ebd8945910b2f367552bd5e315a
silenc3502 <gcccompil3r@gmail.com>
2018-03-06 02:20:17 +0900

    Finish Init

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,6 +11,15 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+	PID::Kp = Kp;
+	PID::Ki = Ki;
+	PID::Kd = Kd;
+
+	d_error = i_error = p_error = 0.0;
+	prev_cross_track_error = 0.0;
+	error_sum = 0.0;
+
+	counter = 0;
 }
 
 void PID::UpdateError(double cte) {
